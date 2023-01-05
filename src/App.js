ae80b6313e7433a935d10f775816615c4d90ac4f
vikas <vikaspatil212222@gmail.com>
2023-01-05 19:32:33 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Stories from "./components/stories/stories.js";
 import Login from "./components/Auth/login";
