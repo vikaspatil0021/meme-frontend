5fb5d2a7c3f14ec81d675acd2ef07edd946a8a3c
vikas <vikaspatil212222@gmail.com>
2023-01-21 19:08:24 +0530

    dashboard

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -48,8 +48,6 @@ const Dashboard = (props) => {
 
     return (
         <div>
-        <div className="sticky-top ">
-
             <div className="quickLinks bg-white px-2 py-2">
                 <div className="d-flex ms-2 ms-md-3">
                     <div className="mx-auto" style={{ width: "1370px" }}>
@@ -64,124 +62,131 @@ const Dashboard = (props) => {
 
 
             </div>
-            <div className="d-flex quickLinks bg-white">
-                <div className="d-flex flex-wrap justify-content-between py-3 mx-auto px-3" style={{ width: "1390px" }}>
-                    <h1 className="fw-bolder fs-2 my-auto ms-1 ms-md-2">
-                        Dashboard
-                    </h1>
+            <div className="sticky-top ">
 
-                    <div className="my-auto">
-                        <a href="/compose" class="btn btn-primary me-1 rounded-4">Compose</a>
-                        <a href="https://my-blog-backend-1ict.onrender.com/logout" class="btn btn-danger rounded-4 me-2 me-md-4">Log out</a>
+
+                <div className="d-flex quickLinks bg-white">
+                    <div className="d-flex flex-wrap justify-content-between py-3 mx-auto px-3" style={{ width: "1390px" }}>
+                        <h1 className="fw-bolder fs-2 my-auto ms-1 ms-md-2">
+                            Dashboard
+                        </h1>
+                        {(loading) ?
+                            <div className="my-auto">
+                                <a href="/compose" class="btn btn-primary me-1 rounded-4 opacity-75">Compose</a>
+                                <a href="https://my-blog-backend-1ict.onrender.com/logout" class="btn btn-danger rounded-4 me-2 me-md-4 opacity-75">Log out</a>
+                            </div> : null}
                     </div>
                 </div>
-            </div>
+                <div class="nav-pills p-2 pb-0 list-group list-group-horizontal" id="pills-tab" role="tablist"  style={{backgroundColor: "#f5f5f7"}}>
+                    <button class="active list-group-item opacity-75 rounded-4 me-2" id="pills-account-tab" data-bs-toggle="pill" data-bs-target="#pills-account" type="button" role="tab" aria-controls="pills-account" aria-selected="true">Account</button>
+                    <button class="list-group-item opacity-75 rounded-4 me-2" id="pills-mystories-tab" data-bs-toggle="pill" data-bs-target="#pills-mystories" type="button" role="tab" aria-controls="pills-mystories" aria-selected="true">My Stories</button>
+                    <button class="list-group-item opacity-75 me-2 rounded-4" id="pills-myfriends-tab" data-bs-toggle="pill" data-bs-target="#pills-myfriends" type="button" role="tab" aria-controls="pills-myfriends" aria-selected="false">My Friends</button>
+
+
+
+                </div>
+
             </div>
 
             {loading ?
                 (<div>
 
-                    <div>
 
 
 
-                        <div class="d-flex align-items-start px-4">
-                            <div class="flex-column nav-pills p-3 col-3 list-group" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                <button class="active list-group-item p-3" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">My Account</button>
-                                <button class="list-group-item p-3" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">My Stories</button>
 
-                            </div>
-                            <div class="tab-content p-3 col-9" id="v-pills-tabContent">
-                                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
-                                    <div class="card mb-2">
-                                        <h2 className="card-title px-4 pt-4">Account info</h2>
-                                        <div class="card-body p-4">
-                                            <div className="mb-4">
-                                                <label className="text-muted pb-2">Name</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label className="text-muted pb-2">Username</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-1">
-                                                <label className="text-muted pb-2">Email</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
-                                            </div>
-
+                    <div class=" px-4">
+                        
+                        <div class="tab-content p-3" id="v-pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-account" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                                <div class="card mb-2">
+                                    <h2 className="card-title px-4 pt-4">Account info</h2>
+                                    <div class="card-body p-4">
+                                        <div className="mb-4">
+                                            <label className="text-muted pb-2">Name</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
+                                            <hr />
                                         </div>
-                                        <div class="card-body p-4">
-                                            <div className="mb-4">
-                                                <label className="text-muted pb-2">Name</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label className="text-muted pb-2">Username</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-1">
-                                                <label className="text-muted pb-2">Email</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
-                                            </div>
-
-                                        </div><div class="card-body p-4">
-                                            <div className="mb-4">
-                                                <label className="text-muted pb-2">Name</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label className="text-muted pb-2">Username</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
-                                                <hr />
-                                            </div>
-                                            <div className="mb-1">
-                                                <label className="text-muted pb-2">Email</label>
-                                                <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
-                                            </div>
-
+                                        <div className="mb-3">
+                                            <label className="text-muted pb-2">Username</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
+                                            <hr />
                                         </div>
+                                        <div className="mb-1">
+                                            <label className="text-muted pb-2">Email</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
+                                        </div>
+
+                                    </div>
+                                    <div class="card-body p-4">
+                                        <div className="mb-4">
+                                            <label className="text-muted pb-2">Name</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
+                                            <hr />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label className="text-muted pb-2">Username</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
+                                            <hr />
+                                        </div>
+                                        <div className="mb-1">
+                                            <label className="text-muted pb-2">Email</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
+                                        </div>
+
+                                    </div><div class="card-body p-4">
+                                        <div className="mb-4">
+                                            <label className="text-muted pb-2">Name</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.name}</h5>
+                                            <hr />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label className="text-muted pb-2">Username</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.username}</h5>
+                                            <hr />
+                                        </div>
+                                        <div className="mb-1">
+                                            <label className="text-muted pb-2">Email</label>
+                                            <h5 class="card-title my-auto">{accountStories.myAccount.email}</h5>
+                                        </div>
+
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="tab-pane fade d-flex row" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
-                                    {accountStories.myStories.map((story) => {
-                                        return (
-                                            <div class="card mt-0 m-1 p-0 col-5">
-                                                <h5 class="card-header py-3">{story.title}</h5>
-                                                <div class="card-body">
-                                                    {/* <hr /> */}
-                                                    <div>
-                                                        <a className="btn py-0 px-2 border-0" href={"/stories/" + story._id}><i class="bi bi-arrow-up-right-square text-info fs-4"></i></a>
-                                                        <a className="btn py-0 px-2 border-0" ><i onClick={() => { props.updateStory(story); navigate("/dashboard/update") }} class="bi bi-pencil-square text-info fs-4"></i></a>
-                                                        <a className="btn py-0 px-2 border-0" href="#" data-bs-toggle="modal" data-bs-target={"#a" + story._id}><i class="bi bi-trash3 text-danger fs-4"></i></a>
+                            <div class="tab-pane fade d-flex row" id="pills-mystories" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                                {accountStories.myStories.map((story) => {
+                                    return (
+                                        <div class="card mt-0 m-1 p-0 col-5">
+                                            <h5 class="card-header py-3">{story.title}</h5>
+                                            <div class="card-body">
+                                                {/* <hr /> */}
+                                                <div>
+                                                    <a className="btn py-0 px-2 border-0" href={"/stories/" + story._id}><i class="bi bi-arrow-up-right-square text-info fs-4"></i></a>
+                                                    <a className="btn py-0 px-2 border-0" ><i onClick={() => { props.updateStory(story); navigate("/dashboard/update") }} class="bi bi-pencil-square text-info fs-4"></i></a>
+                                                    <a className="btn py-0 px-2 border-0" href="#" data-bs-toggle="modal" data-bs-target={"#a" + story._id}><i class="bi bi-trash3 text-danger fs-4"></i></a>
 
 
-                                                        {/* <!-- Modal --> */}
-                                                        <div class="modal fade" id={"a" + story._id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                            <div class="modal-dialog">
-                                                                <div class="modal-content">
-                                                                    <div class="modal-header">
-                                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">{story.title} will be deleted permanentally</h1>
-                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                    </div>
-                                                                    <div class="modal-footer">
-                                                                        <a type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</a>
-                                                                        <a type="button" href="/dashboard" class="btn btn-primary" onClick={() => { deleteStory(story._id) }}>Delete</a>
-                                                                    </div>
+                                                    {/* <!-- Modal --> */}
+                                                    <div class="modal fade" id={"a" + story._id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">{story.title} will be deleted permanentally</h1>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <a type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</a>
+                                                                    <a type="button" href="/dashboard" class="btn btn-primary" onClick={() => { deleteStory(story._id) }}>Delete</a>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        )
-                                    })}
-                                </div>
+                                        </div>
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
