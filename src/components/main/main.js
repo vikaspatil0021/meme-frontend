de9eee255035533e4f22efa23311a4d038af0cc4
vikas <vikaspatil212222@gmail.com>
2023-02-11 19:00:36 +0530

    main

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -53,20 +53,20 @@ const Main = () => {
 
                                     <span className="text-muted  fw-bolder text-left ms-3 ms-md-5" style={{ fontSize: "50px" }}>Welcome to </span>
                                     <p className="text-dark fw-bolder text-left ms-3 ms-md-5" style={{ fontSize: "60px" }}>IMEME,</p>
-                                    <p className="text-muted fw-bolder text-left ms-3 mb-0 ms-md-5" style={{ fontSize: "30px" }}>Ain't you the meme guy? </p>
-                                    <p className="text-muted fw-bolder text-left ms-3 ms-md-5" style={{ fontSize: "30px" }}></p>
+                                    <p className="text-muted fw-bolder text-left ms-3 mb-0 ms-md-5" style={{ fontSize: "25px" }}>Ain't you the meme guy? </p>
+                                    <p className="text-muted fw-bolder text-left ms-3 ms-md-5" style={{ fontSize: "25px" }}></p>
 
 
                                     </div>
 
-                                    <div className="me-md-5 d-flex align-items-end">
+                                    <div id="main-post-button" className="me-md-5 d-flex align-items-end">
 
                                 {(auth) ? <a href="/compose" className="btn btn-primary opacity-75 ms-2 ms-md-5 rounded-5 px-4 mb-4 my-md-4 py-3 fs-4">
                                     Post a meme today!
                                 </a> :
-                                    <a data-bs-toggle="modal" data-bs-target="#authModel" class="btn btn-primary  ms-2 ms-md-5 opacity-75 rounded-5 px-4 mb-4 my-md-4 py-3 fs-4">
+                                    <button data-bs-toggle="modal" data-bs-target="#authModel" class="btn btn-primary  ms-2 ms-md-5 opacity-75 rounded-5 px-4 mb-4 my-md-4 py-3 fs-4">
                                         Post a meme today!
-                                    </a>}
+                                    </button>}
                                     </div>
                                     {/* <div className="d-flex justify-content-center">
 
