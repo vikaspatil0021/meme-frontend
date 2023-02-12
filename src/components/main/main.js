18d28090b957a3ffb3b64432029bb0b51f7e60fc
vikas <vikaspatil212222@gmail.com>
2023-02-12 20:21:19 +0530

    main

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -12,6 +12,7 @@ const Main = () => {
     const [auth, setAuth] = useState('');
     const [loading, setLoading] = useState(false);
     const [storiesData, setStoriesData] = useState([])
+    const [peopleData, setPeopleData] = useState([])
 
     useEffect(() => {
         axios.get(process.env.REACT_APP_SERVER_URL + "/memes", { withCredentials: "include" })
@@ -29,6 +30,16 @@ const Main = () => {
                 console.log(error);
 
             });
+
+        axios.get(process.env.REACT_APP_SERVER_URL + "/people", { withCredentials: "include" })
+            .then((res) => {
+                console.log(res.data);
+                setPeopleData(res.data);
+            }).catch((error) => {
+                console.log(error);
+
+            });
+
         axios.get(process.env.REACT_APP_SERVER_URL + "/isauth", { withCredentials: "include" })
             .then((res) => {
                 setAuth(res.data.isAuth)
@@ -39,21 +50,21 @@ const Main = () => {
 
 
     var prevScrollpos = window.pageYOffset;
-      window.onscroll = function () {
-          var currentScrollPos = window.pageYOffset;
-          if (prevScrollpos > currentScrollPos) {
-              document.getElementById("sticky-top-main").style.top = "0";
-          } else {
-              document.getElementById("sticky-top-main").style.top = "-155px";
-          }
-          prevScrollpos = currentScrollPos;
-      }
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("sticky-top-main").style.top = "0";
+        } else {
+            document.getElementById("sticky-top-main").style.top = "-155px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 
 
-    
+
 
     return (
-        <div  className="">
+        <div className="">
             <div id="sticky-top-main" className="sticky-top bg-white px-2 py-2 border-bottom">
                 <div className="d-flex ms-2 ms-md-3">
                     <div className="mx-auto" style={{ width: "1370px" }}>
@@ -78,14 +89,14 @@ const Main = () => {
             <div className="d-flex">
 
                 <div class="mx-auto mt-2" style={{ width: "1370px" }}>
-                <div className="d-flex justify-content-center ps-2 ps-md-3">
+                    <div className="d-flex justify-content-center ps-2 ps-md-3">
 
 
-                    <div class="nav-pills p-2 pt-0 list-group list-group-horizontal" id="pills-main" role="tablist" >
-                        <button class="active list-group-item opacity-75 rounded-4 me-2" id="pills-account-tab" data-bs-toggle="pill" data-bs-target="#main-memes" type="button" role="tab" aria-selected="true">Memes</button>
-                        <button class="list-group-item opacity-75 rounded-4 me-2" id="pills-mystories-tab" data-bs-toggle="pill" data-bs-target="#main-people" type="button" role="tab" aria-selected="false">People</button>
+                        <div class="nav-pills p-2 pt-0 list-group list-group-horizontal" id="pills-main" role="tablist" >
+                            <button class="active list-group-item opacity-75 rounded-4 me-2" id="pills-account-tab" data-bs-toggle="pill" data-bs-target="#main-memes" type="button" role="tab" aria-selected="true">Memes</button>
+                            <button class="list-group-item opacity-75 rounded-4 me-2" id="pills-mystories-tab" data-bs-toggle="pill" data-bs-target="#main-people" type="button" role="tab" aria-selected="false">People</button>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
 
@@ -103,16 +114,16 @@ const Main = () => {
 
 
 
-                                        <div className="mx-auto px-3 d-flex flex-wrap mw-100">
-                                            {storiesData.map((story) => {
-                                                return (
-                                                    <div className="p-2 pb-0 col-12 col-md-6 col-lg-4">
+                                    <div className="mx-auto px-3 d-flex flex-wrap mw-100">
+                                        {storiesData.map((story) => {
+                                            return (
+                                                <div className="p-2 pb-0 col-12 col-md-6 col-lg-4">
                                                     <a href={"/memes/" + story._id} class="cardLink">
 
                                                         <div class="card Storycard rounded-5">
                                                             <div class="card-content d-flex p-2">
                                                                 <div className="col-4">
-                                                                    <img class=" rounded-5 cropped" src={story.imageURL} alt="Card image cap" height="100" width='120' />
+                                                                    <img class=" rounded-5 cropped" src={story.imageURL} alt="Card image cap" height="100" width='110' />
 
                                                                 </div>
                                                                 <div class="card-body col-8 p-2 ps-3 text-dark">
@@ -126,42 +137,45 @@ const Main = () => {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        </a>
-                                                    </div>)
-                                            })}
+                                                    </a>
+                                                </div>)
+                                        })}
 
 
 
-                                        </div>
-
-                                    <div className="position-fixed w-100 bottom-0 my-3">
-                                    <div className="d-flex justify-content-center">
-
-                                        <a href="/memes" className="btn btn-primary fs-4 py-2 ps-3 rounded-5">Explore memes
-                                        <i className="bi bi-arrow-right fs-4 mx-2" />
-                                        </a>
                                     </div>
+
+                                    <div className="fixed-bottom w-100 bottom-0 my-3">
+                                        <div className="d-flex justify-content-center">
+
+                                            <a href="/memes" className="btn btn-primary fs-4 py-2 ps-3 rounded-5">Explore memes
+                                                <i className="bi bi-arrow-right fs-4 mx-2" />
+                                            </a>
+                                        </div>
 
                                     </div>
 
                                 </div>
 
-                            <div class="tab-pane fade" id="main-people" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
-                            <div className="mx-auto px-3 d-flex flex-wrap mw-100">
-                                            {storiesData.map((story) => {
-                                                return (
-                                                    <div className="p-2 pb-0 col-12 col-md-6 col-lg-4">
-                                                    <a href={"/memes/" + story._id} class="cardLink">
+                                <div class="tab-pane fade" id="main-people" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+                                    <div className="mx-auto px-3 d-flex flex-wrap mw-100">
+                                        {peopleData.map((person) => {
+                                            return (
+                                                <div className="p-2 pb-0 col-12 col-md-6 col-lg-4">
+                                                    <a href={"/people/" + person.username} class="cardLink">
 
                                                         <div class="card Storycard rounded-5">
                                                             <div class="card-content d-flex p-2">
                                                                 <div className="col-4">
-                                                                    <img class=" rounded-5 cropped" src={story.imageURL} alt="Card image cap" height="100" width='120' />
+                                                                    <img class=" rounded-5 cropped" src={person.profileImgURL} alt="Card image cap" height="100" width='100' />
 
                                                                 </div>
-                                                                <div class="card-body col-8 p-2 ps-3 text-dark">
-                                                                    <h5 class="card-title fw-semibold opacity-75" style={{ fontSize: "22px" }}>{story.title}</h5>
+                                                                <div class="card-body col-8 p-2 pb-0 ps-3 text-dark">
+                                                                    <h5 class="card-title fw-semibold" style={{ fontSize: "22px" }}>{person.name}</h5>
+                                                                    <div className="d-block">
 
+                                                                        <p className="card-title d-inline fw-semibold text-muted under-line" style={{ fontSize: "19px" }}>{person.username}</p>
+                                                                    </div>
                                                                     <div className="card-content d-inline-flex flex-wrap rounded-3" style={{ backgroundColor: "#efefef" }}>
 
 
@@ -170,24 +184,24 @@ const Main = () => {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        </a>
-                                                    </div>)
-                                            })}
+                                                    </a>
+                                                </div>)
+                                        })}
 
 
 
+                                    </div>
+
+                                    <div className="fixed-bottom bottom-0 my-3">
+                                        <div className="d-flex justify-content-center">
+
+                                            <a href="/people" className="btn btn-primary fs-4 py-2 ps-3 rounded-5">Explore people
+                                                <i className="bi bi-arrow-right fs-4 mx-2" />
+                                            </a>
                                         </div>
 
-                                    <div className="position-fixed w-100 bottom-0 my-3">
-                                    <div className="d-flex justify-content-center">
-
-                                        <a href="/people" className="btn btn-primary fs-4 py-2 ps-3 rounded-5">Explore people
-                                        <i className="bi bi-arrow-right fs-4 mx-2" />
-                                        </a>
                                     </div>
-
-                                    </div>
-                                                                </div>
+                                </div>
                             </div>
 
                         </div>
